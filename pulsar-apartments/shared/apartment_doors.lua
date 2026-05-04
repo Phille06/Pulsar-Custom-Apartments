5fb06e591dc0ec4a95fee47d37ebc5bbf21fc51e
dev <devonbush348@gmail.com>
2026-05-04 01:04:48 -0400

    Add files via upload

---
+++ pulsar-apartments/shared/apartment_doors.lua
@@ -0,0 +1,546 @@
+-- Each entry id MUST match apartment doorId format: apt_room_<roomNumber>
+-- Not sure if you can add the second building to this list, so only done main for now
+addDoorsListToConfig({
+  -- Room 100
+  {
+    id = "apt_room_100",
+    model = -1028247766,
+    coords = vector3(-459.746399, -938.000488, 47.767715),
+    heading = 270.278503,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 101
+  {
+    id = "apt_room_101",
+    model = -1028247766,
+    coords = vector3(-454.685181, -924.3172, 47.767715),
+    heading = 89.707733,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 102
+  {
+    id = "apt_room_102",
+    model = -1028247766,
+    coords = vector3(-459.746399, -923.930725, 47.767715),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 103
+  {
+    id = "apt_room_103",
+    model = -1028247766,
+    coords = vector3(-454.685181, -910.37439, 47.767715),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 104
+  {
+    id = "apt_room_104",
+    model = -1028247766,
+    coords = vector3(-459.746399, -910.07251, 47.767715),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 105
+  {
+    id = "apt_room_105",
+    model = -1028247766,
+    coords = vector3(-454.685181, -896.462585, 47.767715),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 106
+  {
+    id = "apt_room_106",
+    model = -1028247766,
+    coords = vector3(-459.746399, -896.146423, 47.767715),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 200
+  {
+    id = "apt_room_200",
+    model = -1028247766,
+    coords = vector3(-459.746399, -938.000488, 58.76775),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 201
+  {
+    id = "apt_room_201",
+    model = -1028247766,
+    coords = vector3(-454.685181, -924.3172, 58.76775),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 202
+  {
+    id = "apt_room_202",
+    model = -1028247766,
+    coords = vector3(-459.746399, -923.930725, 58.76775),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 203
+  {
+    id = "apt_room_203",
+    model = -1028247766,
+    coords = vector3(-454.685181, -910.37439, 58.76775),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 204
+  {
+    id = "apt_room_204",
+    model = -1028247766,
+    coords = vector3(-459.746399, -910.07251, 58.76775),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 205
+  {
+    id = "apt_room_205",
+    model = -1028247766,
+    coords = vector3(-454.685181, -896.462585, 58.76775),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 206
+  {
+    id = "apt_room_206",
+    model = -1028247766,
+    coords = vector3(-459.746399, -896.146423, 58.76775),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 300
+  {
+    id = "apt_room_300",
+    model = -1028247766,
+    coords = vector3(-459.746399, -938.000488, 69.767754),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 301
+  {
+    id = "apt_room_301",
+    model = -1028247766,
+    coords = vector3(-454.685181, -924.3172, 69.767754),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 302
+  {
+    id = "apt_room_302",
+    model = -1028247766,
+    coords = vector3(-459.746399, -923.930725, 69.767754),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 303
+  {
+    id = "apt_room_303",
+    model = -1028247766,
+    coords = vector3(-454.685181, -910.37439, 69.767754),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 304
+  {
+    id = "apt_room_304",
+    model = -1028247766,
+    coords = vector3(-459.746399, -910.07251, 69.767754),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 305
+  {
+    id = "apt_room_305",
+    model = -1028247766,
+    coords = vector3(-454.685181, -896.462585, 69.767754),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 306
+  {
+    id = "apt_room_306",
+    model = -1028247766,
+    coords = vector3(-459.746399, -896.146423, 69.767754),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 400
+  {
+    id = "apt_room_400",
+    model = -1028247766,
+    coords = vector3(-459.746399, -938.000488, 80.767776),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 401
+  {
+    id = "apt_room_401",
+    model = -1028247766,
+    coords = vector3(-454.685181, -924.3172, 80.767776),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 402
+  {
+    id = "apt_room_402",
+    model = -1028247766,
+    coords = vector3(-459.746399, -923.930725, 80.767776),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 403
+  {
+    id = "apt_room_403",
+    model = -1028247766,
+    coords = vector3(-454.685181, -910.37439, 80.767776),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 404
+  {
+    id = "apt_room_404",
+    model = -1028247766,
+    coords = vector3(-459.746399, -910.07251, 80.767776),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 405
+  {
+    id = "apt_room_405",
+    model = -1028247766,
+    coords = vector3(-454.685181, -896.462585, 80.767776),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 406
+  {
+    id = "apt_room_406",
+    model = -1028247766,
+    coords = vector3(-459.746399, -896.146423, 80.767776),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 500
+  {
+    id = "apt_room_500",
+    model = -1028247766,
+    coords = vector3(-459.746399, -938.000488, 91.767769),
+    heading = 269.629211,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 501
+  {
+    id = "apt_room_501",
+    model = -1028247766,
+    coords = vector3(-454.685181, -924.3172, 91.767769),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 502
+  {
+    id = "apt_room_502",
+    model = -1028247766,
+    coords = vector3(-459.746399, -923.930725, 91.767769),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 503
+  {
+    id = "apt_room_503",
+    model = -1028247766,
+    coords = vector3(-454.685181, -910.37439, 91.767769),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 504
+  {
+    id = "apt_room_504",
+    model = -1028247766,
+    coords = vector3(-459.746399, -910.07251, 91.767769),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 505
+  {
+    id = "apt_room_505",
+    model = -1028247766,
+    coords = vector3(-454.685181, -896.462585, 91.767769),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 506
+  {
+    id = "apt_room_506",
+    model = -1028247766,
+    coords = vector3(-459.746399, -896.146423, 91.767769),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 600
+  {
+    id = "apt_room_600",
+    model = -1028247766,
+    coords = vector3(-459.746399, -938.000488, 102.767807),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 601
+  {
+    id = "apt_room_601",
+    model = -1028247766,
+    coords = vector3(-454.685181, -924.3172, 102.767807),
+    heading = 17.559883,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 602
+  {
+    id = "apt_room_602",
+    model = -1028247766,
+    coords = vector3(-459.746399, -923.930725, 102.767807),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 603
+  {
+    id = "apt_room_603",
+    model = -1028247766,
+    coords = vector3(-454.685181, -910.37439, 102.767807),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 604
+  {
+    id = "apt_room_604",
+    model = -1028247766,
+    coords = vector3(-459.746399, -910.07251, 102.767807),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 605
+  {
+    id = "apt_room_605",
+    model = -1028247766,
+    coords = vector3(-454.685181, -896.462585, 102.767807),
+    heading = 90.396767,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 606
+  {
+    id = "apt_room_606",
+    model = -1028247766,
+    coords = vector3(-459.746399, -896.146423, 102.767807),
+    heading = 269.611511,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 700
+  {
+    id = "apt_room_700",
+    model = -1028247766,
+    coords = vector3(-459.746399, -938.000488, 108.267815),
+    heading = 233.216675,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 701
+  {
+    id = "apt_room_701",
+    model = -1028247766,
+    coords = vector3(-454.685181, -924.3172, 108.267815),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 702
+  {
+    id = "apt_room_702",
+    model = -1028247766,
+    coords = vector3(-459.746399, -923.930725, 108.267815),
+    heading = 270,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 703
+  {
+    id = "apt_room_703",
+    model = -1028247766,
+    coords = vector3(-454.685181, -910.37439, 108.267815),
+    heading = 89.999985,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 704
+  {
+    id = "apt_room_704",
+    model = -1028247766,
+    coords = vector3(-459.746399, -910.07251, 108.267815),
+    heading = 270.082825,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 705
+  {
+    id = "apt_room_705",
+    model = -1028247766,
+    coords = vector3(-454.685181, -896.462585, 108.267815),
+    heading = 90.355362,
+    locked = true,
+    restricted = {},
+  },
+  -- Room 706
+  {
+    id = "apt_room_706",
+    model = -1028247766,
+    coords = vector3(-459.746399, -896.146423, 108.267815),
+    heading = 269.347839,
+    locked = true,
+    restricted = {},
+  },
+  -- =============================================
+  -- Nexus Apartment Block 2
+  -- Note: door coords are approximated from doorEntry zones.
+  -- Fine-tune in-game if the door model appears offset.
+  -- =============================================
+  -- Floor 1
+  { id = "nexus2_apt_room_100", model = -1028247766, coords = vector3(-493.144, -1039.613, 43.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_101", model = -1028247766, coords = vector3(-479.126, -1039.310, 43.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_102", model = -1028247766, coords = vector3(-477.619, -1048.147, 43.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_103", model = -1028247766, coords = vector3(-463.903, -1047.980, 43.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_104", model = -1028247766, coords = vector3(-465.244, -1039.523, 43.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_105", model = -1028247766, coords = vector3(-449.856, -1048.123, 43.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_106", model = -1028247766, coords = vector3(-451.450, -1039.692, 43.971), heading = 0, locked = true, restricted = {} },
+  -- Floor 2
+  { id = "nexus2_apt_room_200", model = -1028247766, coords = vector3(-493.299, -1039.686, 54.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_201", model = -1028247766, coords = vector3(-477.697, -1047.823, 54.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_202", model = -1028247766, coords = vector3(-479.385, -1039.209, 54.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_203", model = -1028247766, coords = vector3(-463.922, -1047.521, 54.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_204", model = -1028247766, coords = vector3(-465.328, -1039.641, 54.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_205", model = -1028247766, coords = vector3(-449.986, -1047.900, 54.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_206", model = -1028247766, coords = vector3(-451.418, -1039.399, 54.971), heading = 0, locked = true, restricted = {} },
+  -- Floor 3
+  { id = "nexus2_apt_room_300", model = -1028247766, coords = vector3(-493.144, -1039.677, 65.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_301", model = -1028247766, coords = vector3(-477.828, -1048.074, 65.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_302", model = -1028247766, coords = vector3(-477.619, -1048.147, 65.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_303", model = -1028247766, coords = vector3(-463.738, -1047.551, 65.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_304", model = -1028247766, coords = vector3(-465.403, -1039.579, 65.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_305", model = -1028247766, coords = vector3(-449.980, -1047.736, 65.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_306", model = -1028247766, coords = vector3(-451.474, -1039.723, 65.971), heading = 0, locked = true, restricted = {} },
+  -- Floor 4
+  { id = "nexus2_apt_room_400", model = -1028247766, coords = vector3(-493.284, -1039.323, 76.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_401", model = -1028247766, coords = vector3(-477.691, -1047.627, 76.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_402", model = -1028247766, coords = vector3(-479.304, -1039.689, 76.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_403", model = -1028247766, coords = vector3(-463.661, -1047.841, 76.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_404", model = -1028247766, coords = vector3(-465.403, -1039.783, 76.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_405", model = -1028247766, coords = vector3(-449.861, -1047.724, 76.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_406", model = -1028247766, coords = vector3(-451.423, -1039.732, 76.971), heading = 0, locked = true, restricted = {} },
+  -- Floor 5
+  { id = "nexus2_apt_room_500", model = -1028247766, coords = vector3(-493.295, -1039.844, 87.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_501", model = -1028247766, coords = vector3(-477.823, -1047.614, 87.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_502", model = -1028247766, coords = vector3(-479.231, -1039.643, 87.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_503", model = -1028247766, coords = vector3(-463.776, -1047.537, 87.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_504", model = -1028247766, coords = vector3(-465.318, -1039.629, 87.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_505", model = -1028247766, coords = vector3(-449.891, -1047.580, 87.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_506", model = -1028247766, coords = vector3(-451.424, -1039.781, 87.971), heading = 0, locked = true, restricted = {} },
+  -- Floor 6
+  { id = "nexus2_apt_room_600", model = -1028247766, coords = vector3(-493.327, -1039.809, 98.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_601", model = -1028247766, coords = vector3(-477.768, -1047.611, 98.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_602", model = -1028247766, coords = vector3(-479.186, -1039.760, 98.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_603", model = -1028247766, coords = vector3(-463.809, -1047.472, 98.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_604", model = -1028247766, coords = vector3(-465.409, -1039.569, 98.971), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_605", model = -1028247766, coords = vector3(-449.899, -1047.473, 98.971), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_606", model = -1028247766, coords = vector3(-451.635, -1039.710, 98.971), heading = 0, locked = true, restricted = {} },
+  -- Floor 7
+  { id = "nexus2_apt_room_700", model = -1028247766, coords = vector3(-493.335, -1039.903, 104.471), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_701", model = -1028247766, coords = vector3(-477.726, -1047.608, 104.471), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_702", model = -1028247766, coords = vector3(-479.258, -1039.818, 104.471), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_703", model = -1028247766, coords = vector3(-463.857, -1047.650, 104.471), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_704", model = -1028247766, coords = vector3(-465.453, -1039.803, 104.471), heading = 0, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_705", model = -1028247766, coords = vector3(-449.950, -1047.658, 104.471), heading = 180, locked = true, restricted = {} },
+  { id = "nexus2_apt_room_706", model = -1028247766, coords = vector3(-451.438, -1039.882, 104.471), heading = 0, locked = true, restricted = {} },
+})
+
+do
+  local floorZ = {
+    [1]=41.674801,  [2]=45.474818,  [3]=49.274835,  [4]=53.074852,
+    [5]=56.874869,  [6]=60.674886,  [7]=64.474903,  [8]=68.274920,
+    [9]=72.074937,  [10]=75.874954, [11]=79.674971, [12]=83.474988,
+    [13]=87.275005, [14]=91.075022, [15]=94.875039, [16]=98.675056,
+    [17]=102.475073,[18]=106.275090,[19]=110.075107,[20]=113.875124,
+  }
+  -- {roomNum, x, y, heading}
+  local roomLayout = {
+    {1, -825.866, -724.611, 0},   {2, -831.466, -724.611, 0},
+    {3, -837.066, -724.611, 0},   {4, -842.666, -724.611, 0},
+    {5, -838.820, -721.390, 180}, {6, -833.220, -721.390, 180},
+    {7, -827.620, -721.390, 180},
+    {8, -819.983, -704.989, 90},  {9, -819.983, -699.389, 90},
+    {10,-819.983, -693.789, 90},  {11,-819.983, -688.189, 90},
+    {12,-819.983, -682.589, 90},
+    {13,-816.610, -686.526, 270}, {14,-816.610, -692.126, 270},
+    {15,-816.610, -697.726, 270}, {16,-816.610, -703.326, 270},
+    {17,-816.610, -708.926, 270}, {18,-816.610, -714.526, 270},
+    {19,-816.570, -720.126, 270},
+  }
+  local wiwangDoors = {}
+  for floor = 1, 20 do
+    local z = floorZ[floor] + 0.15
+    for _, r in ipairs(roomLayout) do
+      local label = floor * 100 + r[1]
+      table.insert(wiwangDoors, {
+        id      = string.format("map_wiwang_hotel_room_%s", label),
+        model   = 1186698616,
+        coords  = vector3(r[2], r[3], z),
+        heading = r[4],
+        locked  = true,
+        restricted = {},
+      })
+    end
+  end
+  addDoorsListToConfig(wiwangDoors)
+end
